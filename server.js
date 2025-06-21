b5a8c86468397137ac955e818373b499abd4e8f1
panzer726 <campuuus7@gmail.com>
2025-06-21 15:41:18 +0800

    fixed

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
 const path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public'))); // serve HTML/CSS/JS
+const app = express();
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'main.html'));
+  res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
